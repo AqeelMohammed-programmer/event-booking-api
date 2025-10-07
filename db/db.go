e9416a93938e2b83b3eb1038a3fbec1dc49f0126
NULL <aqeelprogrammer@gmail.com>
2025-10-08 01:37:12 +0300

    createing the first table in database

--- db/db.go
+++ db/db.go
@@ -17,9 +17,30 @@ func InitDB() {
 	DB, err = sql.Open("sqlite3", "api.db")
 
 	if err != nil {
-		log.Fatalln("ERROR: cannot initialize the database")
+		panic("Could not connect to database.")
 	}
 
-	DB.SetMaxOpenConns(MAX_OPEN_CONNECTIONS)
-	DB.SetMaxIdleConns(MAX_IDLE_CONNECTIONS)
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+
+	createTables()
+}
+
+func createTables() {
+	createEventsTable := `
+	CREATE TABLE IF NOT EXISTS events (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		description TEXT NOT NULL,
+		location TEXT NOT NULL,
+		dateTime DATETIME NOT NULL,
+		user_id INTEGER
+	)
+	`
+
+	_, err := DB.Exec(createEventsTable)
+
+	if err != nil {
+		log.Fatalln("ERROR: cannot execute the query!!!", err)
+	}
 }
--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"github.com/AqeelMohammed-programmer/event-booking-api/db"
 	"github.com/AqeelMohammed-programmer/event-booking-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
