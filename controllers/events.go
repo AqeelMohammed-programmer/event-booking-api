292f228d18bcc20598c936f8ba1f8cf652c5e81d
NULL <aqeelprogrammer@gmail.com>
2025-10-10 04:08:51 +0300

    feat: implement delete event

--- controllers/events.go
+++ controllers/events.go
@@ -119,3 +119,39 @@ func UpdateEvent(context *gin.Context) {
 		"message": "event updated succsufully.",
 	})
 }
+
+func DeleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "could not parse the event id!",
+		})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not fetch the event!",
+		})
+		return
+	}
+
+	if event == nil {
+		context.JSON(http.StatusNotFound, gin.H{
+			"message": "event not found.",
+		})
+		return
+	}
+
+	err = event.Delete()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "cannot delete the event.",
+		})
+		return
+	}
+
+	context.JSON(http.StatusNoContent, "")
+}
