c0dcbaa372f8967d1956b6994c09e91ec3407d05
NULL <aqeelprogrammer@gmail.com>
2025-10-10 03:22:20 +0300

    feat: implement update event

--- controllers/events.go
+++ controllers/events.go
@@ -71,3 +71,51 @@ func CreateEvent(context *gin.Context) {
 		"message": "Event created!", "event": event,
 	})
 }
+
+func UpdateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "could not parse the event id!",
+		})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Could not fetch the event!",
+		})
+		return
+	}
+
+	if event == nil {
+		context.JSON(http.StatusNotFound, gin.H{
+			"message": "event not found.",
+		})
+		return
+	}
+
+	var updatedEvent models.Event
+	err = context.ShouldBindJSON(&updatedEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": "Could not parse requested date",
+		})
+	}
+
+	updatedEvent.ID = eventId
+	err = updatedEvent.Update()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "cannot update this event",
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"message": "event updated succsufully.",
+	})
+}
