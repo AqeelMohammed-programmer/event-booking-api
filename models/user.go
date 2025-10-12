b4bd7deb876c27d90d1ce69e273f02c98dc80556
NULL <aqeelprogrammer@gmail.com>
2025-10-12 15:35:35 +0300

    feat: add hashing user password when create user

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/AqeelMohammed-programmer/event-booking-api/db"
+import (
+	"github.com/AqeelMohammed-programmer/event-booking-api/db"
+	"github.com/AqeelMohammed-programmer/event-booking-api/utils"
+)
 
 type User struct {
 	ID       int64
@@ -18,7 +21,13 @@ func (u *User) Save() error {
 
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPass, err := utils.HashPassword(u.Password)
+
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPass)
 	if err != nil {
 		return err
 	}
